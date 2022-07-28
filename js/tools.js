f8d740c082d0f2a5c4892964a5fb0dc8d4472ce0
htmltest <struev.vs@gmail.com>
2022-07-28 09:38:30 +0300

    updates
    общая страница

--- js/tools.js
+++ js/tools.js
@@ -1,65 +1,77 @@
 $(document).ready(function() {
 
-    var audioEl = $('audio')[0];
-    var maxTime = 0;
-    var curTime = 0;
+    $('audio').each(function() {
 
-    var isPlaying = false;
+        var audioEl = this;
+        var curAudio = $(this).parent();
 
-    audioEl.onloadedmetadata = function() {
-        maxTime = audioEl.duration;
-    }
+        curAudio.data('isPlaying', false);
 
-    audioEl.ontimeupdate = function() {
-        maxTime = audioEl.duration;
+        audioEl.ontimeupdate = function() {
+            var maxTime = audioEl.duration;
+            var sec_num = audioEl.currentTime;
+            var hours = Math.floor(sec_num / 3600);
+            var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+            var seconds = sec_num - (hours * 3600) - (minutes * 60);
+            seconds = Math.round(seconds);
 
-        var sec_num = audioEl.currentTime;
-        var hours = Math.floor(sec_num / 3600);
-        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-        var seconds = sec_num - (hours * 3600) - (minutes * 60);
-        seconds = Math.round(seconds);
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+            if (seconds < 10) {
+                seconds = '0' + seconds;
+            }
+            curAudio.find('.play-time').html(minutes + ':' + seconds);
 
-        if (minutes < 10) {
-            minutes = '0' + minutes;
+            if (curAudio.data('isPlaying')) {
+                curTime = audioEl.currentTime;
+            }
+
+            curAudio.find('.audio-progress-current').css({'width': (curTime / maxTime * 100) + '%'});
         }
-        if (seconds < 10) {
-            seconds = '0' + seconds;
-        }
-        $('.play-time').html(minutes + ':' + seconds);
 
-        if (isPlaying) {
-            curTime = audioEl.currentTime;
-        }
-
-        $('.audio-progress-current').css({'width': (curTime / maxTime * 100) + '%'});
-    }
-
-    audioEl.onended = function() {
-        audioEl.currentTime = 0;
-        audioEl.pause();
-        isPlaying = false;
-        $('.audio').removeClass('playing');
-        $('.audio-progress-current').css({'width': '0%'});
-    }
-
-    $('.play-btn').click(function() {
-        if (isPlaying) {
+        audioEl.onended = function() {
+            audioEl.currentTime = 0;
             audioEl.pause();
-            isPlaying = false;
-            $('.audio').removeClass('playing');
-        } else {
-            audioEl.play();
-            isPlaying = true;
-            $('.audio').addClass('playing');
+            curAudio.data('isPlaying', false);
+            curAudio.removeClass('playing');
+            curAudio.find('.audio-progress-current').css({'width': '0%'});
         }
-    });
 
-    $('.audio-progress').click(function(e) {
-        var curPos = (e.pageX - $('.audio-progress').offset().left) / $('.audio-progress').width() * maxTime;
-        audioEl.currentTime = curPos;
-        audioEl.play();
-        isPlaying = true;
-        $('.audio').addClass('playing');
+        curAudio.find('.play-btn').click(function() {
+            if (curAudio.data('isPlaying')) {
+                audioEl.pause();
+                curAudio.data('isPlaying', false);
+                curAudio.removeClass('playing');
+            } else {
+                stopAllAudio();
+                audioEl.play();
+                curAudio.data('isPlaying', true);
+                curAudio.addClass('playing');
+            }
+        });
+
+        curAudio.find('.audio-progress').click(function(e) {
+            stopAllAudio();
+            var maxTime = audioEl.duration;
+            var curPos = (e.pageX - curAudio.find('.audio-progress').offset().left) / curAudio.find('.audio-progress').width() * maxTime;
+            audioEl.currentTime = curPos;
+            audioEl.play();
+            curAudio.data('isPlaying', true);
+            curAudio.addClass('playing');
+        });
+
     });
 
 });
+
+function stopAllAudio() {
+    $('audio').each(function() {
+        var audioEl = this;
+        var curAudio = $(this).parent();
+
+        audioEl.pause();
+        curAudio.data('isPlaying', false);
+        curAudio.removeClass('playing');
+    });
+}
